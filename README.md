14d8c77653fbbc06e002b3ba3d9703b928514a33
Kaiconure <leilepilot@hotmail.com>
2025-07-21 16:44:08 -0700

    Adding support for the auto-sell list.

--- README.md
+++ README.md
@@ -12,3 +12,22 @@ Items are removed from queue once selling has completed.
 Will not try to sell items equipped, in bazaar or that can not be sold to NPC vendors.
 
 ##### If there has been a mainenance or update to the game make sure windowers resource files have been updated to reflect new items added.
+
+### Auto-sell
+
+[Kaiconure](https://github.com/Kaiconure) created a [fork of SellNPC](https://github.com/Kaiconure/SellNPC) in July of 2025, which adds support for a managed auto-sell list. This allows you to maintain a list of items that are always sold automatically anytime you interact with a vendor npc.
+
+For command format takes the following form:
+
+```bash
+sellnpc auto [<command>] [<arguments>]
+```
+
+Here's a list of the currently supported commands:
+
+- `on` - This turns the auto-sell feature on. Run as `sellnpc auto on` or view the current status with `sellnpc auto`.
+- `off` - This turns the auto-sell feature off. Run as `sellnpc auto off` or view the current status with `sellnpc auto`.
+- `list` - This lists all items you have in your auto-sell list. Run as `sellnpc auto list`.
+- `add <item_name>` - This adds an item to your auto-sell list. Run as `sellnpc auto add bone chip`.
+- `remove <item_name>` - This removes an item from your auto-sell list. Run as `sellnpc auto remove beehive chip`.
+
--- SellNPC.lua
+++ SellNPC.lua
@@ -1,13 +1,43 @@
 _addon.command = 'SellNPC'
-_addon.version = '2.0.0.3'
-_addon.author = 'Ivaar'
+_addon.version = '2.1.202507.21'
+_addon.author = 'Kaiconure,Ivaar'
 _addon.name = 'SellNPC'
 
 require('sets')
 profiles = require('profiles')
 res_items = require('resources').items
 
+files = require('files')
+json = require('jsonlua')
+
 sales_que = {}
+
+default_settings    = { auto = false, auto_list = {} }
+settings            = nil
+settings_file       = nil
+
+function read_json_from_file(path, defaults)
+    local file = files.new(path)
+    if not file:exists() then
+        return defaults, false
+    end
+
+    return json.parse(file:read()) or defaults
+end
+
+function write_json_to_file(path, obj)
+    local file = files.new(path)
+    file:write(json.stringify(obj))
+end
+
+function colorize(color, message, returnColor)
+    color = color or 72
+    returnColor = returnColor or color
+
+    return string.char(0x1E, tonumber(color)) 
+        .. (message or '')
+        .. string.char(0x1E, returnColor)
+end
 
 function get_item_res(item)
     for k,v in pairs(res_items) do
@@ -17,6 +47,14 @@ function get_item_res(item)
     end
     return nil
 end
+
+function is_valid_item(name)
+    local name = windower.convert_auto_trans(name):lower()
+    local item = get_item_res(name)
+
+    return item
+end
+
 
 function check_item(name, silent)
     local name = windower.convert_auto_trans(name):lower()
@@ -30,9 +68,23 @@ function check_item(name, silent)
     end
 end
 
+function merge_into_sales_que(item_list)
+    for _id, val in pairs(item_list) do
+        local id = tonumber(_id)
+        sales_que[id] = true
+    end
+end
+
 function sell_all_items()
+    -- If we're using the auto list, merge those items into the sales queue now
+    if settings and settings.auto and settings.auto_list then
+        merge_into_sales_que(settings.auto_list)
+    end
+
     local num = 0
-    for index = 1, 80 do local item = windower.ffxi.get_items(0,index)
+    for index = 1, 80 do 
+        local item = windower.ffxi.get_items(0,index)
+        
         if item and sales_que[item.id] and item.status == 0 then
             windower.packets.inject_outgoing(0x084,string.char(0x084,0x06,0,0,item.count,0,0,0,item.id%256,math.floor(item.id/256)%256,index,0))
             windower.packets.inject_outgoing(0x085,string.char(0x085,0x04,0,0,1,0,0,0))
@@ -51,9 +103,123 @@ function initialize_shop(id, data)
     end
 end
 
+function sell_npc_auto_command(command, ...)
+    if settings and settings_file then
+        command = string.lower(tostring(command) or '')
+
+        if not settings.auto_list then
+            settings.auto_list = {}
+        end
+        
+        local commands = {...}
+        local save_changes = false
+        if command == 'on' then
+            settings.auto = true
+            save_changes = true
+
+            windower.add_to_chat(207, '%s: The auto-sell list is: %s':format(_addon.name, colorize(2, 'enabled')))
+        elseif command == 'off' then
+            settings.auto = false
+            save_changes = true
+
+            windower.add_to_chat(207, '%s: The auto-sell list is: %s':format(_addon.name, colorize(76, 'enabled')))
+        elseif command == 'list' then
+            --
+            -- Writes out the auto sell list to the chat log
+            --
+            local num_total = 0
+            local num_added = 0
+            local message = '%s: %s':format(_addon.name, colorize(89, 'Auto drop list: ', 89))
+
+            for _id, val in pairs(settings.auto_list) do
+                local id = tonumber(_id)
+                if id then
+                    local item = res_items[id]
+                    if item then
+                        if num_added > 0 and (num_added % 5) == 0 then
+                            windower.add_to_chat(207, message .. '\n')
+                            message = '  '
+                            num_added = 0
+                        end
+
+                        message = message .. '%s  ':format(colorize(2, item.name, 89))
+                        num_added = num_added + 1
+                        num_total = num_total + 1
+                    end
+                end
+            end
+
+            if num_added > 0 or num_total == 0 then
+                windower.add_to_chat(207, message)
+            end
+        elseif command == 'add' then
+            --
+            -- Adds an item to the auto sell list
+            --
+            local name = table.concat(commands, ' ')
+            local item = is_valid_item(name)
+            if item then
+                windower.add_to_chat(207, '%s: adding "%s" to the auto-sell list.':format(_addon.name, item.name))
+                settings.auto_list[item.id] = {id = item.id, name = item.name}
+
+                save_changes = true
+            else
+                windower.add_to_chat(207, '%s: "%s" not a valid item name.':format(_addon.name, name))
+            end
+        elseif command == 'remove' then
+            --
+            -- Removes an item from the auto sell list
+            --
+            local name = table.concat(commands, ' ')
+            local item = is_valid_item(name)
+            if item then
+                windower.add_to_chat(207, '%s: Removing "%s" (%d) from the auto-sell list.':format(_addon.name, item.name, item.id))
+                settings.auto_list[item.id] = nil
+                
+                save_changes = true
+            else
+                windower.add_to_chat(207, '%s: "%s" not a valid item name.':format(_addon.name, name))
+            end
+        else
+            windower.add_to_chat(207, '%s: The auto-sell list is: %s':format(
+                _addon.name,
+                settings.auto and colorize(2, 'enabled') or colorize(76, 'disabled')
+            ))
+        end
+
+        if save_changes then
+            write_json_to_file(settings_file, settings)
+        end
+    end
+end
+
+function sell_npc_login(name)
+    if type(name) == 'string' then
+        settings_file   = './data/%s.json':format(name)
+        settings        = read_json_from_file(settings_file, default_settings)        
+    end
+end
+
+function sell_npc_logout(name)
+    settings        = nil
+    settings_file   = nil
+end
+
+function sell_npc_load()
+    -- On load, if the player is already logged in we will forward the event to the login handler
+    local player = windower.ffxi.get_player()
+    if player then
+        sell_npc_login(player.name)
+    end
+end
+
 function sell_npc_command(...)
     local commands = {...}
     if not commands[1] then
+    elseif string.lower(commands[1]) == 'auto' then
+        -- Clear the 'auto' entry and forward the remainder of the arguments to the auto handler
+        table.remove(commands, 1)
+        sell_npc_auto_command(table.unpack(commands))
     elseif profiles[commands[1]] then
         for name in pairs(profiles[commands[1]]) do
             check_item(name, true)
@@ -66,3 +232,6 @@ end
 
 windower.register_event('incoming chunk', initialize_shop)
 windower.register_event('addon command', sell_npc_command)
+windower.register_event('login', sell_npc_login)
+windower.register_event('logout', sell_npc_logout)
+windower.register_event('load', sell_npc_load)
